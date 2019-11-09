39174925bbb605a774f0d80ee8938653833e01bb
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-11-09 13:20:01 -0500

    wrote the spotify function

--- liri.js
+++ liri.js
@@ -14,6 +14,7 @@ switch (input) {
         break;
     case ("spotify-this-song"):
         console.log("spotify")
+        getSong(details)
         break;
     case ("movie-this"):
         console.log("movie-this");
@@ -39,4 +40,26 @@ function getConcert(input) {
                 console.log("Date: " + moment(date).format('MM/DD/YYYY [at] hh:mma'));
             }
         })
+}
+
+function getSong(input) {
+    if (!input) {
+        input = "The Sign ace of base";
+    }
+    spotify.search({
+        type: 'track',
+        query: input,
+        limit: 5
+
+    }, function(err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err)
+        }
+
+
+        console.log('ARTIST: ' + data.tracks.items[0].artists[0].name);
+        console.log('LINK: ' + data.tracks.items[0].external_urls.spotify);
+        console.log(data.tracks.items[0].album.name);
+
+    })
 }
